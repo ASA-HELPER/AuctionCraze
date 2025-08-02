08c901bbd998d0aa2a28dbb55e3f01b2d2924898
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2025-08-02 22:32:01 +0530

    Install dependencies and fixed node issue

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import colors from 'colors';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import fileUpload from 'express-fileupload';
+import cloudinary from 'cloudinary';
+
+const app = express();
+dotenv.config({path:"./config/.env"});
+
+// middlewares
+app.use(morgan('dev'))
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(express.json())
+app.use(cors({
+    origin: [process.env.FRONTEND_URL],
+    methods:["GET","POST","DELETE","PUT"],
+    credentials: true
+}))
+app.use(fileUpload({
+    useTempFiles:true,
+    tempFileDir:'/tmp/'
+}))
+
+// Routers
+
+// Database Connection
+
+// custom middlewares
+
+// cloudinary
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server listening at port ${process.env.PORT}.`.bgCyan.white);
+})
+
+export default app;
