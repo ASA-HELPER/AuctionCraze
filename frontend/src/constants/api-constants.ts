12505fba0b2c1529078612ce3f51e6da738ed191
Ayush Verma <vayush2020@gmail.com>
2025-09-06 18:44:35 +0530

    Auction api functions

--- frontend/src/constants/api-constants.ts
+++ frontend/src/constants/api-constants.ts
@@ -1,8 +1,17 @@
-export const API_URL = 'https://auction-craze-backend.vercel.app';
-export const API_ROUTES_PREFIX = 'api/v1';
-export const USER = 'user';
-export const LOGIN = 'login';
-export const LOGOUT = 'logout';
-export const REGISTER = 'register';
-export const PROFILE = 'profile';
-export const LEADERBOARD = 'leaderboard';
+export const API_URL = "https://auction-craze-backend.vercel.app";
+export const API_ROUTES_PREFIX = "api/v1";
+export const USER = "user";
+export const LOGIN = "login";
+export const LOGOUT = "logout";
+export const REGISTER = "register";
+export const PROFILE = "profile";
+export const LEADERBOARD = "leaderboard";
+export const AUCTION_ITEM = "auctionitem";
+export const AUCTION_ROUTES = {
+  CREATE: "create",
+  ALL_ITEMS: "allitems",
+  MY_ITEMS: "myitems",
+  AUCTION_DETAILS: "auction",
+  DELETE_ITEM: "delete",
+  REPUBLISH_ITEM: "item/republish"
+};
