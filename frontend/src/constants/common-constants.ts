9afd84fe4373415269fd36879e3e4ff9e2dcce4e
Ayush Verma <69920055+Ayush-Verm-a@users.noreply.github.com>
2025-09-21 15:05:29 +0530

    register page update (#23)
    * register page update
    
    * New register changes

--- frontend/src/constants/common-constants.ts
+++ frontend/src/constants/common-constants.ts
@@ -1,10 +1,14 @@
 export enum ButtonVariant {
-    Contained = 'contained', // this is small-case as mui components has pre-defined values
-    Outlined = 'outlined',
-    Text = 'text',
+  Contained = "contained", // this is small-case as mui components has pre-defined values
+  Outlined = "outlined",
+  Text = "text",
 }
 
 export enum VariantTypes {
-    Outlined = 'outlined', // this is small-case as mui components has pre-defined values
-    Standard = 'standard',
+  Outlined = "outlined", // this is small-case as mui components has pre-defined values
+  Standard = "standard",
 }
+
+export const BANKNAMES = ["HDFC", "ICICI", "AXIS"];
+
+export const ROLES = ["Auctioneer", "Bidder"];
--- frontend/src/pages/register/Register.tsx
+++ frontend/src/pages/register/Register.tsx
@@ -1,10 +1,191 @@
-import React from 'react'
-import './register-styles.scss'
+import React, { useState } from "react";
+import "./register-styles.scss";
+import CustomInput from "../../components/input/CustomInput";
+import { InputPresets, InputVariant } from "../../constants/input-constants";
+import { UserRole } from "../../types/api-types";
+import { Typography } from "@mui/material";
+import { register } from "../../store/slices/userSlice";
+import { useAppDispatch } from "../../hooks/storeHooks";
+import CustomDropdown from "../../components/dropdown/CustomDropdown";
+import { BANKNAMES, ROLES } from "../../constants/common-constants";
+import registerCopy from "./register.copy";
+import CustomButton from "../../components/button/CustomButton";
+import AvatarPreview from "../../assets/AvatarPreview.png";
 
 const Register = () => {
-  return (
-    <div className='register__container'>Register</div>
-  )
-}
+  const dispatch = useAppDispatch();
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPasswrod] = useState("");
+  const [role, setRole] = useState<UserRole>();
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+  const [bankAccountNumber, setBankAccountNumber] = useState<string>("");
+  const [bankName, setBankName] = useState<string | null>("");
+  const [bankAccountUserName, setBankAccountUserName] = useState("");
+  const [easypaisaAccountNumber, setEasypaisaAccountNumber] = useState("");
+  const [paypalEmail, setPaypalEmail] = useState("");
+  const [profileImage, setProfileImage] = useState<File | null>(null);
 
-export default Register
+  const profileHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        setProfileImage(file);
+      };
+    }
+  };
+
+  const handleRegister = () => {
+    const formData = new FormData();
+    formData.append("userName", userName);
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("phone", phone);
+    formData.append("address", address);
+    formData.append("role", String(role));
+    if(profileImage){
+      formData.append("profileImage", profileImage);
+    }
+    if(role === ROLES[0]){
+      formData.append("bankAccountNumber", bankAccountNumber);
+      formData.append("bankName", String(bankName));
+      formData.append("bankAccountName", bankAccountUserName)
+      formData.append("easypaisaAccountNumber", easypaisaAccountNumber);
+      formData.append("paypalEmail", paypalEmail);
+    }
+    console.log(formData);
+    dispatch(register(formData));
+  };
+
+  return (
+    <div className="register__container">
+      <Typography className="register__title">Register</Typography>
+      <div className="register__subcontainer">
+        <div className="register__imageContainer">
+          <div className="register__imageSubcontainer">
+            <Typography className="register__imgLabels">
+              Profile Image
+            </Typography>
+            <img
+              src={
+                profileImage
+                  ? URL.createObjectURL(profileImage)
+                  : AvatarPreview
+              }
+              alt="avatar"
+              className="register__image"
+            />
+            <div className="relative">
+              <input
+                type="file"
+                onChange={profileHandler}
+                className="avatar-update-btn"
+              />
+            </div>
+          </div>
+        </div>
+        <CustomInput
+          value={userName}
+          handleChange={(e) => setUserName(e.target.value)}
+          preset={InputPresets.Text}
+          variant={InputVariant.Outlined}
+          hasBorder
+          label={registerCopy.userName}
+          inputLabelClass="register__inputLabels"
+        />
+        <CustomInput
+          value={email}
+          handleChange={(e) => setEmail(e.target.value)}
+          preset={InputPresets.Email}
+          variant={InputVariant.Outlined}
+          hasBorder
+          label={registerCopy.email}
+          inputLabelClass="register__inputLabels"
+        />
+        <CustomInput
+          value={password}
+          handleChange={(e) => setPasswrod(e.target.value)}
+          preset={InputPresets.Password}
+          variant={InputVariant.Outlined}
+          hasBorder
+          label={registerCopy.password}
+          inputLabelClass="register__inputLabels"
+        />
+        <CustomDropdown
+          data={ROLES}
+          placeholder={registerCopy.selectRole}
+          setValue={(value) => setRole(value)}
+          value={role}
+          label={registerCopy.role}
+        />
+        <CustomInput
+          value={phone}
+          handleChange={(e) => setPhone(e.target.value)}
+          preset={InputPresets.Text}
+          variant={InputVariant.Outlined}
+          hasBorder
+          label={registerCopy.phone}
+          inputLabelClass="register__inputLabels"
+        />
+        <CustomInput
+          value={address}
+          handleChange={(e) => setAddress(e.target.value)}
+          preset={InputPresets.Text}
+          variant={InputVariant.Outlined}
+          hasBorder
+          label={registerCopy.address}
+          inputLabelClass="register__inputLabels"
+        />
+        <CustomDropdown
+          data={BANKNAMES}
+          placeholder={registerCopy.selectBankName}
+          setValue={(value) => setBankName(value)}
+          value={bankName}
+          label={registerCopy.bankName}
+        />
+        <CustomInput
+          value={bankAccountNumber}
+          handleChange={(e) => setBankAccountNumber(e.target.value)}
+          preset={InputPresets.Text}
+          variant={InputVariant.Outlined}
+          hasBorder
+          label={registerCopy.bankAccountNumber}
+          inputLabelClass="register__inputLabels"
+        />
+        <CustomInput
+          value={bankAccountUserName}
+          handleChange={(e) => setBankAccountUserName(e.target.value)}
+          preset={InputPresets.Text}
+          variant={InputVariant.Outlined}
+          hasBorder
+          label={registerCopy.bankAccountUserName}
+          inputLabelClass="register__inputLabels"
+        />
+        <CustomInput
+          value={easypaisaAccountNumber}
+          handleChange={(e) => setEasypaisaAccountNumber(e.target.value)}
+          preset={InputPresets.Text}
+          variant={InputVariant.Outlined}
+          hasBorder
+          label={registerCopy.easyPaisaAccountNumber}
+          inputLabelClass="register__inputLabels"
+        />
+        <CustomInput
+          value={paypalEmail}
+          handleChange={(e) => setPaypalEmail(e.target.value)}
+          preset={InputPresets.Email}
+          variant={InputVariant.Outlined}
+          hasBorder
+          label={registerCopy.paypalEmail}
+          inputLabelClass="register__inputLabels"
+        />
+        <CustomButton handleClick={handleRegister} title="Register" />
+      </div>
+    </div>
+  );
+};
+
+export default Register;
