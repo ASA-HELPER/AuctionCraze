9ed5d1a9564d398b37170cb09b982a768d757022
Ayush Verma <69920055+Ayush-Verm-a@users.noreply.github.com>
2025-09-28 14:16:03 +0530

    Leaderboard page changes (#26)
    * Leaderboard page changes
    
    * Leaderboard Page updated

---
+++ frontend/src/pages/leaderboard/leaderboard.copy.ts
@@ -0,0 +1,7 @@
+export default {
+    biddersLeaderboard: "Bidder's Leaderboard",
+    profilePic: "Profile Pic",
+    username: "Username",
+    bidExpenditure: "Bid Expenditure",
+    auctionsWon: "Auctions Won"
+}
