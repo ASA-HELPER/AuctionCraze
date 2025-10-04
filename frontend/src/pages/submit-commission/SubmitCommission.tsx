04d942b0d15f0018c5a09c00606a50403af308b8
Ayush Verma <69920055+Ayush-Verm-a@users.noreply.github.com>
2025-10-04 12:01:30 +0530

    Commission page changes (#31)

--- frontend/src/pages/submit-commission/SubmitCommission.tsx
+++ frontend/src/pages/submit-commission/SubmitCommission.tsx
@@ -1,8 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import "./submitCommission-styles.scss";
+import { Typography } from "@mui/material";
+import CustomInput from "../../components/input/CustomInput";
+import {
+  InputPresets,
+  InputVariant,
+} from "../../constants/input-constants";
+import CustomButton from "../../components/button/CustomButton";
+import { useAppDispatch } from "../../hooks/storeHooks";
+import { commissionProof } from "../../store/slices/commissionSlice";
+import submitCommissionCopy from "./submitCommission.copy";
 
 const SubmitCommission = () => {
-  return <div className="submitCommission__container">Submit Commission</div>;
+  const dispatch = useAppDispatch();
+  const [amount, setAmount] = useState("");
+  const [comment, setComment] = useState("");
+  const [proofImage, setProofImage] = useState<File | null>(null);
+
+  const proofHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        setProofImage(file);
+      };
+    }
+  };
+
+  const handleSubmitCommission = () => {
+    const formData = new FormData();
+    formData.append("amount", amount);
+    if (proofImage) {
+      formData.append("proofImage", proofImage);
+    }
+    formData.append("comment", comment);
+    dispatch(commissionProof(formData));
+  };
+
+  return (
+    <div className="submitCommission__container">
+      <Typography className="submitCommission__title" variant="h2">
+        {submitCommissionCopy.uploadPaymentProof}
+      </Typography>
+      <div className="submitCommission__subcontainer">
+        <CustomInput
+          value={amount}
+          handleChange={(e) => setAmount(e.target.value)}
+          preset={InputPresets.Text}
+          variant={InputVariant.Outlined}
+          hasBorder
+          label={submitCommissionCopy.amount}
+          inputLabelClass="submitCommission__inputLabels"
+        />
+        <div className="submitCommission__proofContainer">
+          <Typography className="submitCommission__proofTitle">
+            {submitCommissionCopy.paymentProof}
+          </Typography>
+          <div>
+            <input type="file" onChange={proofHandler} />
+          </div>
+        </div>
+        <CustomInput
+          handleChange={(e) => setComment(e.target.value)}
+          label={submitCommissionCopy.comments}
+          value={comment}
+          preset={InputPresets.Text}
+          variant={InputVariant.Outlined}
+          hasBorder
+        />
+        <CustomButton
+          handleClick={handleSubmitCommission}
+          title={submitCommissionCopy.uploadPaymentProof}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SubmitCommission;
