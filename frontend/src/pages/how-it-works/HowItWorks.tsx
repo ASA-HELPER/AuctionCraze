ff433bc5cb661a1712496770a356634dc7d70ccc
KafkaTamura1310 <dhruvpanjwani1310@gmail.com>
2025-10-04 14:54:31 +0530

    howitworks (#29)
    * howitworks
    
    * How it works updated
    
    ---------
    
    Co-authored-by: Ayush Verma <vayush2020@gmail.com>

--- frontend/src/pages/how-it-works/HowItWorks.tsx
+++ frontend/src/pages/how-it-works/HowItWorks.tsx
@@ -1,8 +1,72 @@
 import React from "react";
 import "./howItWorks-styles.scss";
+import PersonIcon from "@mui/icons-material/Person";
+import GavelIcon from "@mui/icons-material/Gavel";
+import EmailIcon from "@mui/icons-material/Email";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import ReceiptLongIcon from "@mui/icons-material/ReceiptLong";
+import RepeatRoundedIcon from "@mui/icons-material/RepeatRounded";
+import { Typography } from "@mui/material";
+import { SvgIconComponent } from "@mui/icons-material";
+import howItWorksCopy from "./howItWorks.copy";
 
 const HowItWorks = () => {
-  return <div className="howItWorks__container">How It Works</div>;
+  const howItWorksItem = (
+    heading: string,
+    description: string,
+    Icon: SvgIconComponent
+  ) => (
+    <div>
+      <Icon />
+      <div className="howItWorks__textContainer">
+        <Typography variant="body1" className="howItWorks__heading">
+          {heading}
+        </Typography>
+        <Typography variant="body1" className="howItWorks__description">
+          {description}
+        </Typography>
+      </div>
+    </div>
+  );
+  return (
+    <div className="howItWorks__container">
+      <div className="howItWorks__header">
+        <Typography className="howItWorks__title">{howItWorksCopy.title}</Typography>
+      </div>
+      <div className="howItWorks__content">
+        {howItWorksItem(
+          howItWorksCopy.heading1,
+          howItWorksCopy.description1,
+          PersonIcon
+        )}
+        {howItWorksItem(
+          howItWorksCopy.heading2,
+          howItWorksCopy.description2,
+          GavelIcon
+        )}
+        {howItWorksItem(
+          howItWorksCopy.heading3,
+          howItWorksCopy.description3,
+          EmailIcon
+        )}
+        {howItWorksItem(
+          howItWorksCopy.heading4,
+          howItWorksCopy.description4,
+          AttachMoneyIcon
+        )}
+        {howItWorksItem(
+          howItWorksCopy.heading5,
+          howItWorksCopy.description5,
+          ReceiptLongIcon
+        )}
+        {howItWorksItem(
+          howItWorksCopy.heading6,
+          howItWorksCopy.description6,
+          RepeatRoundedIcon
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default HowItWorks;
---
+++ frontend/src/pages/how-it-works/howItWorks.copy.ts
@@ -0,0 +1,21 @@
+export default {
+  title: "Discover How AuctionCraze Works",
+  heading1: "user Registration",
+  description1:
+    "Users must register or log in to perform operations such as posting auctions, bidding on items, accessing the dashboard, and sending payment proof.",
+  heading2: "Role Selection",
+  description2:
+    "Users can register as either a Bidder or Auctioneer. Bidders can bid on items, while Auctioneers can post items.",
+  heading3: "Winning Bid Notification",
+  description3:
+    "After winning an item. the highest bidder will receive an email with the Auctioneer's payment method information, including bank transfer, Easypaisa, and PayPal.",
+  heading4: "Commission Payment",
+  description4:
+    "If the Bidder pays, the Auctioneer must pay 5% of that payment to the platform. Failure to pay results in being unable to post new items, and a legal notice will be sent.",
+  heading5: "Proof of Payment",
+  description5:
+    "The platform receives payment proof as a screenshot and the total amount sent. Once approved by the Administrator, the unpaid commission of the Auctioneer will be adjusted accordingly.",
+  heading6: "Reposting Items",
+  description6:
+    "If the Bidder does not pay, the Auctioneer can republish the item without any additional cost.",
+};
