cce9d14c9cea6faef7f94c88b5214db17c04da9f
Ayush Verma <vayush2020@gmail.com>
2025-11-25 20:18:18 +0530

    profile page style updated

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -28,7 +28,6 @@ const Profile = () => {
       <Typography variant="h3" className="profile__title">Profile</Typography>
       <div className="profile__subcontainer">
         <div className="profile__imageContainer">
-          <div className="profile__imageSubcontainer">
             <Typography className="profile__imgLabels">
               Profile Image
             </Typography>
@@ -41,7 +40,6 @@ const Profile = () => {
               alt="avatar"
               className="profile__image"
             />
-          </div>
         </div>
         <CustomInput
           value={user && user.userName}
