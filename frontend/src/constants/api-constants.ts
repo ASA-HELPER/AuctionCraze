e2a97bec617dcaef2f167c385f425446dcea22c2
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2025-12-24 15:06:17 +0530

    Shifting backend deployment from vercel to render

--- frontend/src/constants/api-constants.ts
+++ frontend/src/constants/api-constants.ts
@@ -1,4 +1,4 @@
-export const API_URL = "https://auction-craze-backend.vercel.app";
+export const API_URL = "https://auctioncraze.onrender.com";
 export const API_ROUTES_PREFIX = "api/v1";
 export const USER = "user";
 export const ADMIN = "admin";
