c82b22913ffafb0cc83aa8148550f2a095795bca
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2025-12-28 13:35:49 +0530

    Changing SMTP service configuration

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,11 +4,11 @@ export const sendEmail = async ({ email, subject, message }) => {
   const transporter = nodeMailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    service: process.env.SMTP_SERVICE,
     auth: {
       user: process.env.SMTP_MAIL,
       pass: process.env.SMTP_PASSWORD,
     },
+    connectionTimeout: 100000,
   });
   const options = {
     from: process.env.SMTP_MAIL,
