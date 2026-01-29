9cfc0d3c946299b9f8044ea57a02382cd7df27be
Amandeep Singh <96967202+ASA-HELPER@users.noreply.github.com>
2026-01-29 20:27:11 +0530

    Fix contact page field validations

--- frontend/src/utils/sendEmail.ts
+++ frontend/src/utils/sendEmail.ts
@@ -1,13 +1,15 @@
-import emailjs from '@emailjs/browser';
-import { ITemplateParams } from '../types/common-types';
+import emailjs from "@emailjs/browser";
+import { ITemplateParams } from "../types/common-types";
 
-export const sendEmail = async (templateParams: ITemplateParams): Promise<boolean> => {
+export const sendEmail = async (
+  templateParams: ITemplateParams,
+): Promise<boolean> => {
   try {
     await emailjs.send(
       import.meta.env.VITE_SERVICE_ID,
       import.meta.env.VITE_TEMPLATE_ID,
       templateParams,
-      import.meta.env.VITE_PUBLIC_KEY
+      import.meta.env.VITE_PUBLIC_KEY,
     );
     return true;
   } catch (err) {
